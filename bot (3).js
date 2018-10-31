49ffb932d159b86e8546bdbe1a2fc5feaf819bd5
Riverr1 <44487673+Riverr1@users.noreply.github.com>
2018-10-31 22:16:32 +0400

    Add files via upload

---
+++ bot (3).js
@@ -0,0 +1,41 @@
+client.on('ready', () => {
+	console.log(----------------);
+	   console.log(Desert Bot- Script By : EX Clan);
+		 console.log(----------------);
+	   console.log(ON ${client.guilds.size} Servers '     Script By : EX Clan ');
+	 console.log(----------------);
+   console.log(Logged in as ${client.user.tag}!);
+ client.user.setGame(افجر ابوك .,"http://twitch.tv/Dream%22)
+ client.user.setStatus("dnd")
+ });
+
+ const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+client.login("MzMyNTQ5ODY4MjE4MjIwNTU2.DrQYfw._5QUnwGIe7vC2oHSxUXc70JTEg4")
